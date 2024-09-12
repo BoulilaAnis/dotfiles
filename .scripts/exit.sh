2d7b9b7eb0d65a22830d941bfab53182ea17e840
Anis Boulila <boulilaa247@gmail.com>
2024-09-12 01:13:39 +0100

    add scripts directory

---
+++ .scripts/exit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+choice=$(echo -e "Shutdown\nReboot\nExit" | dmenu)
+
+
+[ $choice = "Shutdown" ] && doas poweroff
+[ $choice = "Reboot"   ] && doas reboot
+[ $choice = "Exit"     ] && pkill dwm
